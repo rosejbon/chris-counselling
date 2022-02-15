e291845f6de78659570eb0acf6cc7049fa8a3392
roseb.04 <rose@neontribe.co.uk>
2022-02-15 12:02:19 +0000

    add css reset

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
-import { ThemeProvider } from "@theme-ui/core"
-import { theme } from "../theme"
+import { ThemeProvider } from "@theme-ui/core";
+import { theme } from "../theme";
+import "../global.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
     </ThemeProvider>
-  )
+  );
 }
-export default MyApp
+export default MyApp;
