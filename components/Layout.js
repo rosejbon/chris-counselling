9c8fb870afbbf2b4e4601da39705fcc7bbf28447
roseb.04 <rose@neontribe.co.uk>
2022-02-22 11:20:23 +0000

    fix: layout not taking up the whole page

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,46 @@
-import { Box } from "@theme-ui/components";
+import { Box, Flex } from "@theme-ui/components";
 import { Header } from "./Header";
 import { Stack } from "./Stack";
 import { Footer } from "./Footer";
 
 export const Layout = ({ children }) => {
   return (
-    <Box bg="#f7f1ed">
-      <Box
+    <>
+      <Box sx={{ borderBottom: "solid", borderColor: "blue" }}>
+        <Box
+          sx={{
+            margin: "auto",
+            maxWidth: "900px",
+            padding: "1rem",
+          }}
+        >
+          <Header />
+        </Box>
+      </Box>
+      <Flex
+        bg="offwhite"
+        py={2}
         sx={{
-          margin: "auto",
-          maxWidth: "900px",
-          padding: "1rem",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          minHeight: "100vh",
         }}
       >
-        <header>
-          <Header />
-        </header>
-        <main>
-          <Stack gap={5}>{children}</Stack>
-        </main>
-      </Box>
-      <Footer />
-    </Box>
+        <Box>
+          <Box
+            sx={{
+              margin: "auto",
+              maxWidth: "900px",
+              padding: "1rem",
+            }}
+          >
+            <main>
+              <Stack gap={5}>{children}</Stack>
+            </main>
+          </Box>
+        </Box>
+        <Footer />
+      </Flex>
+    </>
   );
 };
