eeb46ccfb7220981caf66d5f9b977db4ef4c76db
roseb.04 <rose@neontribe.co.uk>
2022-03-01 16:12:02 +0000

    highlight page users are on

--- components/Header.js
+++ components/Header.js
@@ -1,58 +1,85 @@
-import { Text, Flex, NavLink, Link } from "theme-ui";
+import { Text, Flex, NavLink, Link, Box } from "theme-ui";
+import { useRouter } from "next/router";
 
-export const Header = () => (
-  <Flex
-    mb={4}
-    sx={{
-      flexDirection: ["column", "column", "row"],
-      justifyContent: "space-between",
-      alignItems: ["flex-start", "flex-end", "flex-end"],
-    }}
-  >
-    <Flex
-      sx={{
-        flexDirection: "column",
-        alignItems: "flex-start",
-        width: "100%",
-      }}
-    >
-      <Link variant="home" href="/">
-        <Text
-          as="h1"
+export const Header = () => {
+  const currentPage = useRouter().pathname;                                                                       
+
+  return (
+    <header>
+      <Flex
+        bg="white"
+        sx={{
+          flexDirection: ["column", "column", "row"],
+          justifyContent: "space-between",
+          alignItems: ["flex-start", "flex-end", "flex-end"],
+        }}
+      >
+        <Flex
           sx={{
-            fontSize: 4,
-            fontWeight: "bold",
-            textDecoration: "none",
-            color: "primary",
-            fontFamily: "heading",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            width: "100%",
           }}
         >
-          Chris Davis <br />
-          <Text sx={{ fontFamily: "heading" }}>COUNSELLING</Text>
-        </Text>
-      </Link>
-    </Flex>
-    <Flex
-      as="nav"
-      pt={3}
-      sx={{
-        width: "100%",
-        justifyContent: ["space-between", "space-between", []],
-        flexDirection: "column",
-        "@media screen and (min-width: 22em)": {
-          flexDirection: "row",
-        },
-      }}
-    >
-      <NavLink href="counselling" pt={2}>
-        Counselling
-      </NavLink>
-      <NavLink href="supervision" pt={2}>
-        Supervision
-      </NavLink>
-      <NavLink href="contact" pt={2}>
-        Contact
-      </NavLink>
-    </Flex>
-  </Flex>
-);
+          <Link variant="home" href="/">
+            <Text
+              as="h1"
+              sx={{
+                fontSize: 4,
+                fontWeight: "bold",
+                textDecoration: "none",
+                color: "offblack",
+                fontFamily: "heading",
+              }}
+            >
+              Chris Davis <br />
+              <Text sx={{ fontFamily: "heading", color: "burntorange" }}>
+                COUNSELLING
+              </Text>
+            </Text>
+          </Link>
+        </Flex>
+        <Flex
+          as="nav"
+          pt={3}
+          sx={{
+            width: "100%",
+            justifyContent: ["space-between", "space-between", []],
+            flexDirection: "column",
+            "@media screen and (min-width: 22em)": {
+              flexDirection: "row",
+            },
+          }}
+        >
+          <NavLink
+            href="counselling"
+            pt={2}
+            sx={{
+              color: currentPage.includes("counselling") ? "burntorange" : null,
+            }}
+          >
+            Counselling
+          </NavLink>
+          <NavLink
+            href="supervision"
+            pt={2}
+            sx={{
+              color: currentPage.includes("supervision") ? "burntorange" : null,
+            }}
+          >
+            Supervision
+          </NavLink>
+          <NavLink
+            href="contact"
+            pt={2}
+            sx={{
+              color: currentPage.includes("contact") ? "burntorange" : null,
+            }}
+          >
+            Contact
+          </NavLink>
+        </Flex>
+      </Flex>
+    </header>
+  );
+};
